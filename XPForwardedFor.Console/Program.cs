bc84604e6d3e2a02159a4089ea7b5673c02e9966
IRiver666 <hoaivinp@gmail.com>
2025-06-10 18:34:08 +0700

    Update Program.cs

--- XPForwardedFor.Console/Program.cs
+++ XPForwardedFor.Console/Program.cs
@@ -10,9 +10,9 @@ string baseData = @"{
     ""codec_fingerprint"":"""",
     ""audio_fingerprint"":"""",
     ""audio_properties"":null,
-    ""created_at"":0
+    ""created_at"":1748492990477
 }";
 string mainKey = "0e6be1f1e21ffc33590b888fd4dc81b19713e570e805d4e5df80a493c9571a05";
-string guestId = "guest-id";
+string guestId = "v1%3A174849298500261196";
 string encrypted = XPForwardedFor.GenerateXP.Encrypt(baseData, mainKey, guestId);
 Console.WriteLine(encrypted);
